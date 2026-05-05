e73cd0805e061362f27c0fa79c852ce387c6bf31
Ricardo Gerardi <ricardo.gerardi@gmail.com>
2026-05-05 08:42:37 -0400

    Adds noctalia shell

--- build_files/build.sh
+++ build_files/build.sh
@@ -99,7 +99,10 @@ repo_gpgcheck=0
 gpgcheck=1
 gpgkey=https://packages.smallstep.com/keys/smallstep-0x889B19391F774443.gpg
 EOT
-dnf makecache && dnf install -y step-cli
+dnf5 makecache && dnf5 install -y step-cli
+
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 -y install noctalia-shell
 
 dnf5 clean all
 
