fc003406b951b8a9afb18c98448bb40c1677ba70
Ricardo Gerardi <ricardo.gerardi@gmail.com>
2026-05-09 17:58:51 -0400

    Updates dnf command to keep it standard

--- build_files/build.sh
+++ build_files/build.sh
@@ -99,7 +99,7 @@ repo_gpgcheck=0
 gpgcheck=1
 gpgkey=https://packages.smallstep.com/keys/smallstep-0x889B19391F774443.gpg
 EOT
-dnf makecache && dnf install -y step-cli
+dnf5 makecache && dnf5 install -y step-cli
 
 dnf5 clean all
 
