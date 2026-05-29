42719b44e5e420fc7224e11fa08d6f65f3a0c24f
Ricardo Gerardi <ricardo.gerardi@gmail.com>
2026-05-28 23:17:32 -0400

    Adds rpmfusion appstream metadata

--- build_files/build.sh
+++ build_files/build.sh
@@ -115,6 +115,7 @@ dnf5 -y config-manager disable tailscale-stable
 
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
+dnf5 -y install rpmfusion-\*-appstream-data
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf5 -y install mesa-va-drivers-freeworld libva-utils
